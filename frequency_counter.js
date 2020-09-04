d9913ad1fb43a2112061a081d99ca75f9f20079b
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-09-04 14:50:31 -0700

    init

---
+++ frequency_counter.js
@@ -0,0 +1,6 @@
+// given two strings check to see if the frequency of the characters match.
+
+function validAnagram(str1, str2){
+  // iterate over both strings and store frequency in object
+  // loop over one object and check that all strings match
+}
