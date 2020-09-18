ad51679869c35f4c0d59080ec4996b1104ab437c
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-09-18 14:24:07 -0700

    finished

--- minSubArrayLen.js
+++ minSubArrayLen.js
@@ -1,27 +1,30 @@
-// write a function that finds the minimal length for a sub array that has a sum equal or more than target.
+// write a function that returns the length of the samllest subarr that has a sum equal or more than the target value
 
-// idea: find the first subarray that is qualified. then iterate the array
-    // add the right element and remove the left
-        // if above target, move left
-        // if below target, move right
-    // we have to keep track of the smallest length of arr
+// idea: iterate through the arr as a sub array
+    // have to pointers left/right
+    // we know we hit the end of the iteration when left is all the way to the end
+    // keep track of the smallest length
     
     let minSubArrayLen = (arr, target) => {
+      let left = 0;
+      let right = 0;
+      let length = Infinity;
       let total = 0;
-      let i = 0;
-      while (total < target) {
-          if (i > arr.length) return 0
-          total += arr[i];
-          i++;
-      }
-      let length = i + 1;
-      for (let j = i + 1; j < arr.length; j++) {
-          let tempTotal = total + arr[j] - arr[length - j];
-          if (tempTotal < target) {
-              // how many times do we need to add
+      // iterate while left is less than arr.length
+      while (left < arr.length) {
+          if (total < target && right < arr.length) {
+              // we add an element to the right
+              total += arr[right];
+              right++;
+          } else if (total >= target){
+              // we remove an element from the left
+              total -= arr[left];
+              left++;
+              length = Math.min(length, right - left + 1);
+              if (left === arr.length && total < target) return 0;
+          } else {
+              break;
           }
-          if (tempTotal > target) {
-              // we dont know how many times can we move 
-          }
       }
+      return length === Infinity ? 0 : length;
   }
