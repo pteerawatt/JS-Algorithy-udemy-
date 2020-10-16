5ca47e1baa59e05a9bf1ccb6542ab8a515dc88a4
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-10-16 16:29:30 -0700

    working

--- raidx_sort.js
+++ raidx_sort.js
@@ -17,3 +17,30 @@ const digitCount = (num) => {
   if (num === 0) return 1;
   return Math.floor(Math.log10(Math.abs(num))) + 1;
 }
+
+const maxDigits = (nums) => {
+  let max = 0;
+  for (let e of nums) max = Math.max(digitCount(e), max);
+  return max;
+}
+// console.log(maxDigits([1,23, 12345, 22, 2, 13451]))
+
+const radixSort = (nums) => {
+  // find max digit 
+  const maxdigits = maxDigits(nums); 
+  // loop over max digt ammount of time
+    // create bucket
+    // place element element in bucket
+    // concat all arrays in bucket
+  for (let t = 0; t < maxdigits; t++) {
+    let bucket = Array.from({length: 10}, () => [])
+    for (let i = 0; i < nums.length; i++) {
+       let num = getDigit(nums[i], t);
+       bucket[num].push(nums[i]);
+    }
+    nums = [].concat(...bucket);
+  }
+  
+  
+  return nums
+}
