98d4adb9ca9b7062df2bb187bf4146297fc2fa66
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-10-16 15:06:50 -0700

    get digit from a position

---
+++ raidx_sort.js
@@ -0,0 +1,13 @@
+
+
+
+
+// helper function allows us to get a value at a position. like index but start from the right side
+
+// 7323, 2
+// 7323/100 = 73.23
+// floor => 73
+// 73 % 10 = 3
+const getDigit = (num, i) => {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+};
