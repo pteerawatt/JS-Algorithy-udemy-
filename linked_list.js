cf2c6f5bf0b3710bfce5fa14278a947eb1fa9aa2
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-10-19 13:33:50 -0700

    push working

---
+++ linked_list.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+}
+
+let LL = new SinglyLinkedList();
+LL.push(1);
+console.log(LL.head === LL.tail)
