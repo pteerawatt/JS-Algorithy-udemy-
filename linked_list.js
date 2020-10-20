2b7e43cadbca53db2e132dca10e01dc0ada7f1cf
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-10-20 14:38:33 -0700

    insert working

--- linked_list.js
+++ linked_list.js
@@ -79,10 +79,35 @@ class SinglyLinkedList {
     let count = 0;
     let current = this.head;
     while (current) {
-      if (count === i) return current.val;
-      current = current.next;
+      if (count === i) return current;
+      current = current.next; 
       count++;
     }
+  }
+
+  // set a node value
+  set(i, val) {
+    let foundNode = this.get(i);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  // insert a node at the given index
+  insert(i, val) {
+    if (i < 0 || i > this.length) return false;
+    if (i === this.length) return !!this.push(val);
+    if (i === 0) return !!this.unshift(val);
+    let priorNode = this.get(i - 1);
+    let afterNode = this.get(i);
+    let insertNode = new Node(val);
+
+    priorNode.next = insertNode;
+    insertNode.next = afterNode;
+    this.length++;
+    return true;
   }
 }
  
@@ -100,4 +125,4 @@ LL.shift()
 LL.shift()
 LL.unshift(1)
 LL.unshift(2)
-console.log(LL.get(2))
+console.log(LL.insert(1, 99))
