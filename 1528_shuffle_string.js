0851b57d0dba601e1b045da94770a278faa6eb52
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-10-23 15:26:56 -0700

    working solution

---
+++ 1528_shuffle_string.js
@@ -0,0 +1,16 @@
+// Given a string s and an integer array indices of the same length.
+
+// The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+// Return the shuffled string.
+
+// Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+// Output: "leetcode"
+
+var restoreString = function(s, indices) {
+  let result = s.split('');
+  for (i = 0; i < s.length; i++) {
+      result[indices[i]] = s[i] ;
+  }
+  return result.join('');
+};
