4e3e56d26c6ce0b2994b6b3796041ba68f5e3286
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-10-26 14:30:23 -0700

    working but i think we can make it On

---
+++ 1365_how_many_numbers_are_samller.js
@@ -0,0 +1,15 @@
+var smallerNumbersThanCurrent = function(nums) {
+  //     let result = [];
+      
+  //     for (let i = 0; i < nums.length; i++) {
+  //         let counter = 0;
+  //         for (let j = 0; j < nums.length; j++) {
+  //             if (j !== i) {
+  //                 if (nums[i] > nums[j]) counter++;
+  //             }
+  //         }
+  //         result.push(counter);
+  //     }
+  //     return result;
+}
+
