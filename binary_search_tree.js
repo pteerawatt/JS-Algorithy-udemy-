d6ca90c30d2e59244577aad52602d6761d9883cb
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-10-27 13:27:45 -0700

    insert is able to add to root but there is a bug when another element is added

---
+++ binary_search_tree.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert (val) {
+    let newNode = new Node(val)
+    if (!this.root) this.root = newNode;
+    else {
+      let current = this.root;
+      while (current.left || current.right) {
+        if (val < current.val) current = current.left;
+        else current = current.right;
+      }
+      if (val < current.val) current.left = newNode;
+      else current.right = newNode;
+    }
+    return this;
+  }
+}
