ee53acbf4a39969e65c4249f4cb59801b0944d60
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-10-27 15:20:27 -0700

    search function working

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -10,6 +10,7 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
   insert (val) {
     let newNode = new Node(val)
     if (!this.root) this.root = newNode;
@@ -24,5 +25,14 @@ class BinarySearchTree {
       else current.right = newNode;
     }
     return this;
+  }
+
+  search(val, node = this.root) {
+    if (!node) return null;
+    if (node.val === val) return node;
+    else {
+      if (val < node.val) return this.search(val, node.left)
+      else if (val > node.val) return this.search(val, node.right)
+    }
   }
 }
