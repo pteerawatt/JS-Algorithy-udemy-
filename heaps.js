2cad688919314fb0b32d75db3f4cdda7db9680c3
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-11-23 12:13:40 -0800

    implemented insert

--- heaps.js
+++ heaps.js
@@ -21,4 +21,46 @@ class MaxBinaryHeap {
     }
   }
 
+  remove() {
+    // swap first and last value, and remove last value
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+  
+      // correct the tree by sinking
+      this.sinkDown();
+    }
+
+    return max;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if ((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      [this.values[idx], this.values[swap]] = [this.values[swap], this.values[idx]];
+      idx = swap;
+    }
+  }
 }
