a1239076a24553e6491bef7400a0693e43b4409d
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-01 13:06:13 -0800

    improved data distrabution by using prime number to generate total

--- hash_function.js
+++ hash_function.js
@@ -1,8 +1,10 @@
 const hash = (key, arrayLen) => {
   let total = 0;
-  for (let char of key) {
+  let primeNum = 31;
+  for (let i = 0; i < key.length; i++) {
+    let char = key[i];
     let value = char.charCodeAt(0) - 96;
-    total = (total + value) % arrayLen;
+    total = (total * primeNum + value) % arrayLen;
   }
   return total;
 }
