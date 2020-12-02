1fd02e4a9c51cc1ce9d17619a47c49060ab73d20
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-02 11:05:42 -0800

    implemented addedges to make a connection between vertex

--- graph_list.js
+++ graph_list.js
@@ -7,4 +7,9 @@ class Graph {
     // currently duplicates will be replaced. and conditional statement is needed to handle duplicates.
     this.adjacencyList[vertex] = [];
   }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
 }
