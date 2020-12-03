d159512e3134d7db3102b79e3ddf98367ceade70
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-03 12:57:45 -0800

    implemented priority queue and test cases

--- dikstra.js
+++ dikstra.js
@@ -13,9 +13,32 @@ class WeightedGraph {
   }
 }
 
-let G = new WeightedGraph();
-G.addVertex("A");
-G.addVertex("B");
-G.addVertex("C");
-G.addEdge("A", "B", 9);
-G.addEdge("A", "C", 5);
+// // test for weighted graph
+// let G = new WeightedGraph();
+// G.addVertex("A");
+// G.addVertex("B");
+// G.addVertex("C");
+// G.addEdge("A", "B", 9);
+// G.addEdge("A", "C", 5);
+
+
+class PriorityQueue {
+  constructor() {
+    this.queue = [];
+  }
+  enqueue(value, priority) {
+    this.queue.push({ value, priority});
+    this.queue.sort((a, b) => a.priority - b.priority);
+  }
+  dequeue() {
+    return this.queue.shift();
+  }
+}
+
+// //test for priority queue
+// let Q = new PriorityQueue();
+// Q.enqueue("B", 3);
+// Q.enqueue("C", 5);
+// Q.enqueue("D", 2);
+// Q.enqueue("Q", 20);
+// console.log(Q.dequeue());
