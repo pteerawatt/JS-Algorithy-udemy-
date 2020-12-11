844410de6a2c05b71bfb8d21726df80493b464fe
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-12-11 14:07:15 -0800

    improve the algo by storing solutions to sub problems

--- dynamic_programing.js
+++ dynamic_programing.js
@@ -3,6 +3,15 @@
 // fib sequence: 1, 1, 2, 3, 5, 8, 13, ...
 
 const fibSeq = (n) => {
-  if (n <= 2) return 1;
+  if (n < 2) return 1;
   return fibSeq(n - 2) + fibSeq(n - 1) 
+}
+
+// using dynamic programming we can store sub problems so we dont have to keep calculating the same f(n) over and over
+
+const fibSeqDP = (n, sto = {}) => {
+  if (n < 2) return 1;
+  if (sto[n]) return sto[n];
+  sto[n] = fib(n - 2, sto) + fib(n - 1, sto);
+  return sto[n];
 }
